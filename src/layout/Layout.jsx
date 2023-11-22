80b597d4d316bca80f189af71e026a2faa183402
Md. Rabbinur Muktar <59724876+Rabbinur@users.noreply.github.com>
2023-11-22 15:40:08 +0600

    update

--- src/layout/Layout.jsx
+++ src/layout/Layout.jsx
@@ -1,6 +1,7 @@
 import Header from "../components/Header/Header";
 import Footer from "../components/Footer/Footer";
 import Routers from "../routes/Routers";
+
 const Layout = () => {
   return (
     <div>
