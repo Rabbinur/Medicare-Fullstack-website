fe7fdd8528e3e9384cdf786684687a08decea355
Md. Rabbinur Muktar <59724876+Rabbinur@users.noreply.github.com>
2023-11-26 19:10:42 +0600

    feedback about

--- src/components/Pages/Doctors/DoctorAbout.jsx
+++ src/components/Pages/Doctors/DoctorAbout.jsx
@@ -52,6 +52,38 @@ const DoctorAbout = () => {
           </li>
         </ul>
       </div>
+      <div className="mt-12">
+        <h3 className="text-[20px] leading-[30px]  text-headingColor font-semibold">
+          {" "}
+          Experience
+        </h3>
+        <ul className="grid sm:grid-cols-2 gap-[30px] pt-4 md:p-5">
+          <li className="p-4 rounded bg-[#fff9ea]">
+            <span className="text-yellowColor text-[15px] leading-6 font-semibold">
+              {" "}
+              {formateDate("12-04-2010")}- {formateDate("12-04-2013")}
+            </span>
+            <p className="text-[16px] leading-6 font-medium text-textColor">
+              Sr. Surgeon
+            </p>
+            <p className="text-[14px] leading-6 font-medium text-textColor">
+              New Apollo Hospital, New Work
+            </p>
+          </li>
+          <li className="p-4 rounded bg-[#fff9ea]">
+            <span className="text-yellowColor text-[15px] leading-6 font-semibold">
+              {" "}
+              {formateDate("12-04-2010")}- {formateDate("12-04-2013")}
+            </span>
+            <p className="text-[16px] leading-6 font-medium text-textColor">
+              Sr. Surgeon
+            </p>
+            <p className="text-[14px] leading-6 font-medium text-textColor">
+              New Apollo Hospital, New Work
+            </p>
+          </li>
+        </ul>
+      </div>
     </div>
   );
 };
